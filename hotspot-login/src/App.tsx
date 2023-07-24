9437e8dca5269b1caa5c2dd6f9570b3242364da5
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-07-24 10:31:10 +0100

    modified

--- hotspot-login/src/App.tsx
+++ hotspot-login/src/App.tsx
@@ -1,13 +1,9 @@
 import Login from './components/Login'
-// import Login2 from './components/Login2'
-// import Login3 from './components/Login3';
 
 function App() {
   return (
     <div>
       <Login />
-      {/* <Login2 /> */}
-      {/* <Login3 /> */}
     </div>
   );
 }
